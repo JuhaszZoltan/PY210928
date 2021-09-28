03c2d487681d2d70a522872fabb7d2dd8a00d093
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2021-09-28 11:27:13 +0200

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+import math
+
+a = float(input('a = '))
+b = float(input('b = '))
+c = float(input('c = '))
+
+if a == 0 or b == 0 or c == 0:
+  print('ezen egyenlet megoldásánál nem használható a megoldóképlet')
+else:
+  d = math.pow(b, 2) + 4 * a * c
+  if d < 0: 
+    print('valós számok halmazán nincw megoldása az egyenletnek')
+  elif d == 0:
+    print(f'x = {- b / (2 * a)}')
+  else:
+    print(f'x1 = {- b + math.sqrt(d) / (2 * a)}')
+    print(f'x2 = {- b - math.sqrt(d) / (2 * a)}')
+
+print('ennyi kthxbye')
